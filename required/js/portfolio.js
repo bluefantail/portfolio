7ca865c1a53d89b38ad000433718361694f5201d
Matt Fannin <mattfannin@acidic.co.nz>
2016-03-23 13:54:03 +1300

    Finished adding film content, re-organised some sections.

--- required/js/portfolio.js
+++ required/js/portfolio.js
@@ -13,12 +13,15 @@ function handle_click(e){
 			document.querySelector('section#' + id).className = '';
 			document.querySelector('nav a.active').classList.remove('active');
 			e.currentTarget.classList.add('active');
-			// window.scroll(0,0);
 			break;
 	}
 }
 function stop_propigation(e){
 	e.stopPropagation();
+}
+function toggle_profile(e){
+	e.stopPropagation();
+	largeProfile.classList.toggle('hide');
 }
 
 var navItems = document.querySelectorAll('nav a');
@@ -44,6 +47,11 @@ var projectLinks = document.querySelectorAll('#channels *');
 Array.prototype.forEach.call(projectLinks, function(a){
 	a.addEventListener('click', stop_propigation);
 });
+
+var profileImage = document.querySelector('#profile-image');
+	profileImage.addEventListener('click', toggle_profile);
+var largeProfile = document.querySelector('#large-profile');
+	largeProfile.addEventListener('click', toggle_profile);
 
 sectionElements[0].className = '';
 
